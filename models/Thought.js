fec90cf5ba34bb56b198beffc56d40528a3c1bdc
rtj2023 <jiwanmallrebecca32@gmail.com>
2024-05-21 20:17:58 -0400

    add thought

---
+++ models/Thought.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+// Schema to create a thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
